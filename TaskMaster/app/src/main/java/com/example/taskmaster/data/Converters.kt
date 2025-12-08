45de5f5b05abb20442c8d1b2903ecd7a7f447b36
Bharathnaik <bharathnaik.cbs@gmail.com>
2025-12-08 20:10:57 +0530

    Add files via upload

---
+++ TaskMaster/app/src/main/java/com/example/taskmaster/data/Converters.kt
@@ -0,0 +1,8 @@
+package com.example.taskmaster.data
+import androidx.room.TypeConverter
+import java.time.LocalDate
+
+class Converters {
+    @TypeConverter fun fromTimestamp(value: String?): LocalDate? = value?.let { LocalDate.parse(it) }
+    @TypeConverter fun dateToTimestamp(date: LocalDate?): String? = date?.toString()
+}
