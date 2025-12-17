b34dbdf2a2af11976879a3abc099194c79740056
Bharathnaik <bharathnaik.cbs@gmail.com>
2025-12-17 15:23:59 +0530

    Delete app directory

--- app/src/main/java/com/example/taskmaster/data/Converters.kt
+++
@@ -1,8 +0,0 @@
-package com.example.taskmaster.data
-import androidx.room.TypeConverter
-import java.time.LocalDate
-
-class Converters {
-    @TypeConverter fun fromTimestamp(value: String?): LocalDate? = value?.let { LocalDate.parse(it) }
-    @TypeConverter fun dateToTimestamp(date: LocalDate?): String? = date?.toString()
-}
